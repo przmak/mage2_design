d74a802c5f0b73eab5df261134651c3d38206ece
k.wisniewski <k.wisniewski@accenture.com>
2019-01-09 09:56:18 +0100

    raz

--- registration.php
+++ registration.php
@@ -5,6 +5,6 @@
  */
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::THEME,
-    'frontend/sample/ultimate',
+    'frontend/przmak/ultimate',
     __DIR__
 );
